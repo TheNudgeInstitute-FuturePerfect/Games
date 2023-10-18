ed929870a7f59125f4c216cac4b1fa669b7e1808
Aashutosh Chaudhary <aashutosh.chaudhary@thenudge.org>
2023-10-18 14:29:23 +0530

    fix

--- tense-travel/frontend/src/index.js
+++ tense-travel/frontend/src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <StageState>
-    <Router basename={"/game/tense-travel"}>
+    <Router basename={"/games/tense-travel"}>
       <App />
     </Router>
   </StageState>
